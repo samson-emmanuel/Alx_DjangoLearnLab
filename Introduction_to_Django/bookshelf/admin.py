e3cc5e59cdf3efcca8d42009a28e6c99c7579836
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2026-01-10 12:18:26 +0100

    Bookadmin added

---
+++ Introduction_to_Django/bookshelf/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    
+
+# Register your models here.
+admin.site.register(Book)
+
