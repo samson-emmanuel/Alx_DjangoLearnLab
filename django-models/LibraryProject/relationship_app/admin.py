63ad5aa9b9b426b5389ac4f0e731f652182fa61d
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2026-01-18 22:56:30 +0100

    update

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
-# Register your models here.
+# Function to check if user is Admin
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+# Function-based Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from Introduction_to_Django.LibraryProject.bookshelf import views
+from LibraryProject.bookshelf import views
+from LibraryProject.relationship_app.admin import admin_view
 from .views import list_books, LibraryDetailView, register
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -10,4 +11,5 @@ path('library/[int:pk](int:pk)/', LibraryDetailView.as_view(), name='library_det
 path('register/', views.register, name='register'),  # <- matches "views.register"
 path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
 path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+path('admin-view/', admin_view.admin_view, name='admin_view'),
 ]
