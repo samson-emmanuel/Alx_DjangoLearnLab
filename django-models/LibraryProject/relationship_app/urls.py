94085dc80a640674808fd7dfdf3eb21d59f61784
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2026-01-18 23:14:06 +0100

    update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,4 +14,5 @@ path('login/', LoginView.as_view(template_name='relationship_app/login.html'), n
 path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 path('admin-view/', admin_view.admin_view, name='admin_view'),
  path('librarian-view/', librarian_view.librarian_view, name='librarian_view'),
+path('member-view/', views.member_view, name='member_view'),
 ]
