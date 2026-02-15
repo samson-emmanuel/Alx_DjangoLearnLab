da9bbb1c6ffee240ba11e417e53c13b4ac34bb6b
Emmanuel Samson <samsonemmanuel9025@gmail.com>
2026-02-15 18:31:54 +0100

    published date added

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,9 +6,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    tags = TaggableManager()
+    published_date = models.DateTimeField(auto_now_add=True)
+    
+    # updated_at = models.DateTimeField(auto_now=True)
+    # tags = TaggableManager()
 
     def __str__(self):
         return self.title
